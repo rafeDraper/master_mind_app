a23372be4e2f4f84f00544937572124f5f1c434d
rafedraper <rafedraper@gmail.com>
2020-02-05 17:47:40 +0100

    added sessions and views. WIP the code break

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source 'https://rubygems.org'
 
 ruby    '2.7.0'
 gem     'sinatra'
+gem     'sysrandom'
--- master_mind.rb
+++ master_mind.rb
@@ -1,6 +1,24 @@
 require 'sinatra'
-require 'sinatra/reloader' if development?
+require 'sysrandom/securerandom' if development?
+require 'sinatra/reloader'       if development?
+
+configure do
+  enable :sessions
+  set :session_secret, ENV['SESSION_KEY']
+  set :session_secret, SecureRandom.hex(64) if development?
+  set :sessions, expire_after: 2_592_000
+end
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  session[:active_game]
+  redirect params[:role]
+end
+
+get '/codebreaker' do
+  session[:active_game]
+  erb :game
 end
