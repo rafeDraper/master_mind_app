3ca4830f385d5d562dcd9aff2b793af45600e96c
rafedraper <rafedraper@gmail.com>
2020-02-13 14:07:55 +0100

    adding tests for two methods

--- models/mastermind_model.rb
+++ models/mastermind_model.rb
@@ -2,16 +2,19 @@
 
 # class for mastermind game
 class Mastermind
-  def initialize
+  attr_accessor :player_role
+
+  def initialize(player_role)
     @player_role = player_role
-    @lives = 0
-    @player_code = []
+    @lives = 10
   end
 
-  def generate_code
-    code = [rand(9), rand(9), rand(9), rand(9)]
-    code.join('')
+  def generate_code(user_input = 0)
+    user_input_array = user_input.to_s.chars.map(&:to_i)
+    if user_input.zero?
+      Array.new(4) { rand(6) }
+    else
+      user_input_array
+    end
   end
-
-  def compare_code; end
 end
--- spec/mastermind_model_spec.rb
+++ spec/mastermind_model_spec.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
+
 require './models/mastermind_model'
 
 RSpec.describe Mastermind do
+  context 'initializing' do
+    it 'should let the user chose a role' do
+      codemaker = Mastermind.new('codemaker')
+      codebreaker = Mastermind.new('codebreaker')
+      expect(codemaker).to have_attributes(player_role: 'codemaker')
+      expect(codebreaker).to have_attributes(player_role: 'codebreaker')
+    end
 
-  context "creating" do
-    
+    it 'should generate a user code' do
+      game = Mastermind.new('codemaker')
+      code = [1, 1, 2, 3]
+      expect(game.generate_code(1123)).to eq(code)
+    end
+
+    it 'should generate a random code when empty' do
+      game = Mastermind.new('codebreaker')
+      expect(game.generate_code.count).to equal(4)
+    end
   end
-  
 end
